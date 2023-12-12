bd9873ab49293ee6eb966f82330e5feed5bfcd27
Abagiu Razvan <abagiurazvan8989@gmail.com>
2023-12-12 13:51:38 +0200

    fixed retrigger of button, all butons trigger same popup now

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Button } from "react-bootstrap";
 import homieLogo from "./assets/logo-homie.png";
 import React, { useState } from "react";
 import EmailModal from "./components/dialog-component";
-import Navbar from './components/Navbar';
+import Navbar from "./components/Navbar";
 function App() {
   const [showModal, setShowModal] = useState(false);
 
@@ -17,46 +17,61 @@ function App() {
   };
 
   return (
-    <div className='backgroundStyle'>
-      <div className='cropContainter'>
-      <div className='overlayStyle'>
-        <div className='homieTitle'>Homie</div>
-        <div className='subtitleStyles'>
-          Platformă de recenzii pentru proprietari și chiriași <br />
-          Încrederea se câștigă prin transparență
-        </div>
-        <div className='inputContainerStyle'>
-          <Button
-            variant="primary"
-            onClick={handleOpenModal}
-            className="btn-send-email"
-          >
-            Arată-mi recenzii
-          </Button>
+    <div className="backgroundStyle">
+      <div className="cropContainter">
+        <div className="overlayStyle">
+          <div className="homieTitle">Homie</div>
+          <div className="subtitleStyles">
+            Platformă de recenzii pentru proprietari și chiriași <br />
+            Încrederea se câștigă prin transparență
+          </div>
+          <div className="inputContainerStyle">
+            <Button
+              variant="primary"
+              onClick={handleOpenModal}
+              className="btn-send-email"
+            >
+              Arată-mi recenzii
+            </Button>
 
-          <EmailModal
-            showModal={showModal}
-            handleCloseModal={handleCloseModal}
-          />
-
-        </div>
+            <EmailModal
+              showModal={showModal}
+              handleCloseModal={handleCloseModal}
+            />
+          </div>
         </div>
       </div>
-      <div className='navbarStyle'>
+      <div className="navbarStyle">
         <div>
-          <img src={homieLogo} alt="House Icon" className='imageStyle' />
+          <img src={homieLogo} alt="House Icon" className="imageStyle" />
         </div>
-        <div className='buttonContainerStyle'>
-          <Button variant="link" className='buttonLinkStyle'>
+        <div className="buttonContainerStyle">
+          <Button
+            variant="link"
+            className="buttonLinkStyle"
+            onClick={handleOpenModal}
+          >
             Vezi imobile disponibile
           </Button>
-          <Button variant="link" className='buttonLinkStyle'>
+          <Button
+            variant="link"
+            className="buttonLinkStyle"
+            onClick={handleOpenModal}
+          >
             Înregistrează-ți imobilul
           </Button>
-          <Button variant="link" className='buttonLinkStyle'>
+          <Button
+            variant="link"
+            className="buttonLinkStyle"
+            onClick={handleOpenModal}
+          >
             Contact
           </Button>
-          <Button variant="link" className='buttonLinkStyle'>
+          <Button
+            variant="link"
+            className="buttonLinkStyle"
+            onClick={handleOpenModal}
+          >
             Loghează-te
           </Button>
         </div>
--- src/components/dialog-component.js
+++ src/components/dialog-component.js
@@ -1,39 +1,49 @@
 import React from "react";
 import { Modal, Form } from "react-bootstrap";
 import { useForm, ValidationError } from "@formspree/react";
-import './EmailModal.css';
+import "./EmailModal.css";
 
 const EmailModal = ({ showModal, handleCloseModal }) => {
   const [state, handleSubmit] = useForm(
     process.env.REACT_APP_FORMSPREE_FORM_ID
   );
-  if (state.succeeded) {
+
+  const customSubmit = () => {
+    handleSubmit();
     handleCloseModal();
-  } else if (state.errors) {
-    // TODO: handle errors, show another modal and a message
-    handleCloseModal();
-  }
+  };
+
   const labelStyle = {
     display: "block",
-    marginBottom: "5px"
+    marginBottom: "5px",
   };
 
   const textBox = {
     backgroundColor: "#C5AFA7",
-    border: "1px solid #C5AFA7"
-  }
+    border: "1px solid #C5AFA7",
+  };
 
   return (
-    <Modal className='custom-modal' show={showModal} onHide={handleCloseModal} centered>
+    <Modal
+      className="custom-modal"
+      show={showModal}
+      onHide={handleCloseModal}
+      centered
+    >
       <Modal.Header className="modal-header" closeButton>
         <Modal.Title>Te anunțăm când ne lansăm!</Modal.Title>
       </Modal.Header>
 
-      <Modal.Body className='modal-body'>
-        <Form onSubmit={handleSubmit}>
+      <Modal.Body className="modal-body">
+        <Form onSubmit={customSubmit}>
           <Form.Group className="modal-field" controlId="formBasicEmail">
             <Form.Label style={labelStyle}>Email</Form.Label>
-            <Form.Control style={textBox} type="email" placeholder="Scrie email" name="email" />
+            <Form.Control
+              style={textBox}
+              type="email"
+              placeholder="Scrie email"
+              name="email"
+            />
             <Form.Text className="text-muted">
               Îți vom trimite prin e-mail confirmările și notificările.
             </Form.Text>
@@ -45,13 +55,22 @@ const EmailModal = ({ showModal, handleCloseModal }) => {
           </Form.Group>
           <Form.Group className="modal-field" controlId="formBasicName">
             <Form.Label style={labelStyle}>Nume și prenume </Form.Label>
-            <Form.Control style={textBox} type="text" placeholder="Popescu Ion" name="name" />
+            <Form.Control
+              style={textBox}
+              type="text"
+              placeholder="Popescu Ion"
+              name="name"
+            />
             <Form.Text className="text-muted">
               Asigură-te că se potrivește cu numele de pe actul tău de
               identitate.
             </Form.Text>
           </Form.Group>
-          <button className='submit-form-button' type="submit" disabled={state.submitting}>
+          <button
+            className="submit-form-button"
+            type="submit"
+            disabled={state.submitting}
+          >
             Trimite
           </button>
         </Form>
