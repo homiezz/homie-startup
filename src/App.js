c3b46a9b5662f24c6795b1845ef090f784c71fe8
Abagiu Razvan <abagiurazvan8989@gmail.com>
2024-01-03 19:33:22 +0200

    separate logic in firebase.js

--- src/App.js
+++ src/App.js
@@ -5,28 +5,6 @@ import React, { useState } from "react";
 import EmailModal from "./components/dialog-component";
 import { Button } from "react-bootstrap";
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: "homie-12235.firebaseapp.com",
-  projectId: "homie-12235",
-  storageBucket: "homie-12235.appspot.com",
-  messagingSenderId: "739903230811",
-  appId: "1:739903230811:web:65e39cfafb88da9549807e",
-  measurementId: "G-T1E3L1LTCV",
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
 function App() {
   const [showModal, setShowModal] = useState(false);
 
