d01342b41133c294daab99a319b04cf6aa635e84
maria-brinza <mariacristinaa407@gmail.com>
2024-01-10 23:36:12 +0200

    in-progress-profile

--- src/components/profile-page.js
+++ src/components/profile-page.js
@@ -149,216 +149,34 @@ export const ProfilePage = () => {
   };
 
   return (
-    <div className="tenants-profile">
-      <ReviewModal
-        showReviewModal={showReviewModal}
-        handleCloseReviewModal={handleCloseReviewModal}
-      />
-      <AddImageModal
-        showAddImageModal={showAddImageModal}
-        handleCloseAddImageModal={handleCloseAddImageModal}
-        onSaveProfilePic={handleSaveProfilePic}
-      />
-      <div className="overlap-wrapper">
-        <div className="overlap">
-          <img
-            className="background"
-            alt="Background"
-            src="https://c.animaapp.com/3A91v25w/img/background-gradientartboard-1-2.png"
-          />
-          <img
-            className="rectangle"
-            alt="Rectangle"
-            src="https://c.animaapp.com/3A91v25w/img/rectangle-19.svg"
-          />
-          <div className="div" />
-          <div className="group">
-            <label htmlFor="profilePicInput" className="img-container">
-              <img className="img" alt="Group" src={userData.profilePic} />
-              <Button className="buttonStyle" onClick={handlePenClick}>
-                🖊️
-              </Button>
-            </label>
-            <div className="group-2">
-              <div className="overlap-group">
-                <div className="text-wrapper">from</div>
-                <div className="text-wrapper-2">4.23</div>
-              </div>
-              <div className="text-wrapper-3">12 reviews</div>
-              <img
-                className="star"
-                alt="Star"
-                src="https://c.animaapp.com/3A91v25w/img/star-1-3.svg"
-              />
+    <div className="overlayContainerProfile">
+    <div className="profilePageContent">
+      <div className="profileOverlay">
+        <div className="profile-pic-container">
+          <img className="profile-pic-profile" alt="Gg profile" src="https://c.animaapp.com/6XN1UVYO/img/gg-profile.svg" />
+        </div>
+        <div className="reviews-container-profile">
+          <img className="reviews-pic-profile" alt="Star" src="https://c.animaapp.com/6XN1UVYO/img/star-1-1.svg" />
+          <div className="reviews-right-profile">
+            <div className="reviews-grade-profile">
+              4.23
+            </div>
+            <div className="reviews-no">
+              from <b> 12 reviews</b>
             </div>
           </div>
-          <p className="about-you">
-            <span className="span">Despre</span>
-            <span className="text-wrapper-4">&nbsp;</span>
-            <span className="text-wrapper-5">{userData.userName}</span>
-          </p>
-          {/* <p className="p">{description}</p> */}
-          <div className="p">
-            {isEditingDescription ? (
-              <>
-                <input
-                  type="text"
-                  value={description}
-                  onChange={(e) => setDescription(e.target.value)}
-                />
-                <Button
-                  type="button"
-                  className="buttonStyle"
-                  onClick={handleSaveDescription}
-                >
-                  Salveaza
-                </Button>
-              </>
-            ) : (
-              <>
-                <p className="p">{userData.description}</p>
-                <Button
-                  type="button"
-                  className="buttonStyle"
-                  onClick={handleEditDescription}
-                >
-                  🖊️
-                </Button>
-              </>
-            )}
-          </div>
-          <div className="profileDescriptionContainer">
-            {isEditingInterests ? (
-              <>
-                <input
-                  type="text"
-                  value={interests}
-                  onChange={(e) => setInterests(e.target.value)}
-                />
-                <Button
-                  type="button"
-                  className="buttonStyle"
-                  onClick={handleSaveInterests}
-                >
-                  Salveaza
-                </Button>
-              </>
-            ) : (
-              <>
-                {""}
-                <p className="looking-for-a">{userData.interests}</p>
-                <Button
-                  type="button"
-                  className="buttonStyle"
-                  onClick={handleEditInterests}
-                >
-                  🖊️
-                </Button>
-              </>
-            )}
-          </div>
-          {/* <p className="looking-for-a">{interests}</p> */}
-          <p className="member-for-years">
-            <span className="span">member for </span>
-            <span className="text-wrapper-6">2 years</span>
-          </p>
-          <img
-            className="line"
-            alt="Line"
-            src="https://c.animaapp.com/3A91v25w/img/line-1.svg"
-          />
-          <img
-            className="line-2"
-            alt="Line"
-            src="https://c.animaapp.com/3A91v25w/img/line-2.svg"
-          />
-          <div className="overlap-group-wrapper">
-            <div className="overlap-2">
-              <div className="text-wrapper-7">Verified user</div>
-              <img
-                className="vector"
-                alt="Vector"
-                src="https://c.animaapp.com/3A91v25w/img/vector.svg"
-              />
-            </div>
-          </div>
-          <div className="text-wrapper-8">Reviews</div>
-          <img
-            className="line-3"
-            alt="Line"
-            src="https://c.animaapp.com/3A91v25w/img/line-1.svg"
-          />
-          <div className="text-wrapper-9">Interest</div>
-          <div className="div-wrapper">
-            <div className="overlap-3">
-              <div className="group-3">
-                <div className="text-wrapper-10">4.5</div>
-                <img
-                  className="star-2"
-                  alt="Star"
-                  src="https://c.animaapp.com/3A91v25w/img/star-1-2.svg"
-                />
-              </div>
-              <p className="text-wrapper-11">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit
-                amet volutpat consequat mauris nunc congue nisi vitae suscipit.
-                Non curabitur gravida arcu ac tortor dignissim. Mattis vulputate
-                enim nulla aliquet porttitor lacus....
-              </p>
-            </div>
-          </div>
-          <div className="group-4">
-            <div className="overlap-4">
-              <div className="group-3">
-                <div className="text-wrapper-10">4.5</div>
-                <img
-                  className="star-2"
-                  alt="Star"
-                  src="https://c.animaapp.com/3A91v25w/img/star-1-2.svg"
-                />
-              </div>
-              <p className="text-wrapper-11">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit
-                amet volutpat consequat mauris nunc congue nisi vitae suscipit.
-                Non curabitur gravida arcu ac tortor dignissim. Mattis vulputate
-                enim nulla aliquet porttitor lacus....
-              </p>
-            </div>
-            <div className="group-5">
-              <div className="overlap-group-2">
-                <div className="group-3">
-                  <div className="text-wrapper-10">4.5</div>
-                  <img
-                    className="star-2"
-                    alt="Star"
-                    src="https://c.animaapp.com/3A91v25w/img/star-1-2.svg"
-                  />
-                </div>
-                <p className="text-wrapper-11">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+          
+        </div>
+        <div className="description">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
                   do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                   Sit amet volutpat consequat mauris nunc congue nisi vitae
                   suscipit. Non curabitur gravida arcu ac tortor dignissim.
                   Mattis vulputate enim nulla aliquet porttitor lacus....
-                </p>
-              </div>
-            </div>
-          </div>
-          <img
-            className="ep-arrow-left"
-            alt="Ep arrow left"
-            src="https://c.animaapp.com/3A91v25w/img/ep-arrow-left.svg"
-          />
-          <div className="group-6">
-            <Button variant="link" onClick={handleOpenReviewModal}>
-              Adaugă o recenzie
-            </Button>
-          </div>
         </div>
       </div>
     </div>
+  </div>
   );
 };
 
